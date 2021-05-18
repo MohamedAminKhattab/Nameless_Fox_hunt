5eb54e0394836c49118eae168e84787d353039d5
DESKTOP-5VMHJT8\Mohamed Amin Khattab <khattabmohamed174@gmail.com>
2021-05-18 23:16:30 +0200

    added win lose canvases and level result display to ui prefab

---
+++ Assets/___OurForest/Scripts/UI/LevelResult.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelResult : MonoBehaviour
+{
+    [SerializeField] BoolSO playerWon;
+    [SerializeField] BoolSO gameOver;
+    [SerializeField] BoolSO gamePaused;
+    [SerializeField] Canvas clearCanvas;
+    [SerializeField] Canvas lostCanvas;
+    [SerializeField] Canvas gameUI;
+    void Start()
+    {
+        gamePaused.state = false;
+        clearCanvas.gameObject.SetActive(false);
+        lostCanvas.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerWon.state == true)
+        {
+            Time.timeScale = 0.0f;
+            gamePaused.state = true;
+            clearCanvas.gameObject.SetActive(true);
+            gameUI.gameObject.SetActive(false);
+
+        }
+        if (gameOver.state == true)
+        {
+            Time.timeScale = 0.0f;
+            gamePaused.state = true;
+            lostCanvas.gameObject.SetActive(true);
+            gameUI.gameObject.SetActive(false);
+        }
+        if (gameOver.state == false&&playerWon.state==false)
+        {
+            Time.timeScale = 1.0f;
+            gamePaused.state = false;
+            lostCanvas.gameObject.SetActive(false);
+            clearCanvas.gameObject.SetActive(false);
+            gameUI.gameObject.SetActive(true);
+        }
+    }
+}
