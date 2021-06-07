799ab59af505c7d7d5def45b08fb0eb0a49810b2
DESKTOP-5VMHJT8\Mohamed Amin Khattab <khattabmohamed174@gmail.com>
2021-06-07 23:14:44 +0200

    fixed arrow shot hitting enemy event

---
+++ Assets/___OurForest/Scripts/Gameplay/Arrowshots.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrowshots : MonoBehaviour
+{
+
+    [SerializeField] EventSO EnemyDied1;
+    [SerializeField] EventSO EnemyDied2;
+    [SerializeField] EventSO EnemyDied3;
+    [SerializeField] EventSO EnemyDied4;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            other.GetComponent<CapsuleCollider>().enabled = false;
+            string name = other.GetComponentInParent<SpawnPoint>().name;
+            switch (name)
+            {
+                case "Point1":
+                    EnemyDied1.Raise();
+                    break;
+                case "Point2":
+                    EnemyDied2.Raise();
+                    break;
+                case "Point3":
+                    EnemyDied3.Raise();
+                    break;
+                case "Point4":
+                    EnemyDied4.Raise();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
