edff4ab5380754a2f84c6386b9c70a01cb2f6675
DoniaMohamed17 <doniamohamedabdelrahim@gmail.com>
2021-06-09 20:50:10 +0200

    New cinmachine camera

--- Assets/___OurForest/Scripts/Gameplay/CameraFollow.cs
+++ Assets/___OurForest/Scripts/Gameplay/CameraFollow.cs
@@ -13,7 +13,9 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     Vector3 offset;
     Vector3 desiredPosition;
-
+    Vector3 cameraDirection;
+    float cameraDistance;
+    Vector2 cameraMinMax = new Vector2(0.5f, 5f);
     private static CameraFollow instance;
     private void Awake()
     {
@@ -27,13 +29,33 @@ public class CameraFollow : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    void Start()
+    {
+        //cameraDirection = transform.localPosition.normalized;
+        //cameraDistance = cameraMinMax.y;
+
+    }
     void FixedUpdate()
     {
         if (target)
         {
             desiredPosition = target.position + offset;
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, 1 / laziness);
+            transform.position = Vector3.Lerp(transform.position, desiredPosition,1);
             transform.LookAt(target);
         }
+
+    
+    }
+    void CheckCameraOcclusionAndCollision(Transform cam)
+    {
+        Vector3 desiredCameraPosition = transform.TransformPoint(cameraDirection* cameraMinMax.y);
+        RaycastHit hit;
+        if(Physics.Linecast(transform.position,desiredCameraPosition,out hit))
+            cameraDistance = Mathf.Clamp(hit.distance, cameraMinMax.x, cameraMinMax.y);
+      
+        else
+          cameraDistance = cameraMinMax.y;
+        
+        transform.localPosition=cameraDirection*cameraDistance;
     }
 }
