88e13af595eee8c21e11879195f37db3c2259be8
Ahmed.Mohamed <ahmedmohamednaboot@gmail.com>
2021-06-12 23:14:34 +0200

    HealthBar Added please Assign the Sliders to the fox and yelena when u instansiate them.... the 2 sliders are in the Hud Prefab

---
+++ Assets/Enemy Indicator/HealthIndicator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthIndicator : MonoBehaviour
+{
+    public Slider healthBar;
+    [SerializeField] HealthSO health;
+    Vector3 healthBarPos;
+
+
+    void Update()
+    {
+        healthBarPos = Camera.main.WorldToScreenPoint(transform.position);
+        healthBar.value = (int)health.currentHealth;
+        healthBar.transform.position = healthBarPos + new Vector3(0, 90, 0);
+    }
+}
