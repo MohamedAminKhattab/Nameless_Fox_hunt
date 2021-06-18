ea22c1d4478abd3a8dd67419c4c5203ebde59b22
DESKTOP-5VMHJT8\Mohamed Amin Khattab <khattabmohamed174@gmail.com>
2021-06-18 02:21:35 +0200

    work on tutorial ui

--- Assets/___OurForest/Scripts/UI/TutorialIntro.cs
+++ Assets/___OurForest/Scripts/UI/TutorialIntro.cs
@@ -8,13 +8,10 @@ public class TutorialIntro : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] IntegerSO selectedLevel;
     [SerializeField] Canvas VideoIntro;
+    [SerializeField] Canvas hud;
     [SerializeField] EventSO introEnded;
     private void Update()
     {
-        if (selectedLevel.value == 0)
-        {
-            VideoIntro.gameObject.SetActive(true);
-        }
         if (!VideoIntro.GetComponentInChildren<VideoPlayer>().isPlaying)
         {
             introEnded.Raise();
@@ -23,6 +20,12 @@ public class TutorialIntro : MonoBehaviour
     }
     public void PlayVideo()
     {
-       
+        Debug.Log("VideoStarted");
+        if (selectedLevel.value == 0)
+        {
+            VideoIntro.gameObject.SetActive(true);
+            VideoIntro.GetComponentInChildren<VideoPlayer>().Play();
+            hud.gameObject.SetActive(false);
+        }
     }
 }
